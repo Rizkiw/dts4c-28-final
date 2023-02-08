4311c5916b3546c8602d989108540e1eacb001db
Rizki <rizki.wibisono.k@gmail.com>
2023-02-08 17:42:58 +0700

    modifiy responsive

--- src/components/NewsArticles.js
+++ src/components/NewsArticles.js
@@ -40,8 +40,8 @@ function NewsArticles(data) {
         {/* title and description */}
       <Card
         className="news-card-title"
-        onClick={() => openInNewTab(<DetailNewsPage />)}
-        // onClick={() => openInNewTab(showArticle.link)}
+        // onClick={() => openInNewTab(<DetailNewsPage />)}
+        onClick={() => openInNewTab(showArticle.link)}
       >
         <CardActionArea>
         <CardContent>
